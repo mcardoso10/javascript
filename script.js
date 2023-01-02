8b5469b11afc0f071d4f8c02426b339fe5b10974
Adelvanio <adelvanio83@gmail.com>
2023-01-02 19:34:12 -0300

    aula3

---
+++ script.js
@@ -0,0 +1,6 @@
+const saladadefruta = ['morago', 'manga', 'uva', 'maçã'];
+console.log(saladadefruta)
+saladadefruta.push('leite condensado', 'leite em pó')
+console.log(saladadefruta)
+saladadefruta.pop()
+console.log(saladadefruta)
